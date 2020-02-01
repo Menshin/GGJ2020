903e14f902fc6680469b92b9b9d11c53476fa80f
tbottini <thomasbottini@protonmail.com>
2020-02-01 22:37:53 +0100

    map, shader, finish generator

---
+++ Scripts/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public List<Cell> neighbor = new List<Cell>();
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddNeighbour(Cell new_neighbor)
+    {
+    	neighbor.Add(new_neighbor);
+    }
+}
