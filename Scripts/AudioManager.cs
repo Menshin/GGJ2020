6fa87a2be70f00ab3d0149e871bd2d6e73ca78ba
tbottini <thomasbottini@protonmail.com>
2020-02-02 14:58:34 +0100

    add audio file

---
+++ Scripts/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public Planet planet;
+
+	AudioSource animalAudioSource;
+	AudioSource themeAudioSource;
+
+
+	public AudioClip[] clips;
+
+	bool play = true;
+	bool changePiste = false;
+
+    void Start()
+    {
+        animalAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (play && !changePiste)
+    	{
+    		animalAudioSource.Play();
+    	}
+    	else
+    	{
+    		animalAudioSource.Stop();
+    		changePiste = false;
+    	}
+    }
+
+    public void ChangeAudio(int piste)
+    {
+    	/*if (piste < clips.Length)
+    	{
+     		changePiste = true;
+     		AudioSource.clip = clips[piste];
+
+    	}
+    	else
+    	{
+    		Debug.Log("depassement de liste de pistes");
+    	}
+ */
+    }
+}
