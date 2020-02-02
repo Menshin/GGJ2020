6f373bd4e761fda14cf922f32d17160e6ad12314
bprunevi <prunevielle.benjamin@protonmail.com>
2020-02-02 18:12:55 +0100

    Merge Ben
    added interface

---
+++ Scripts/UI/Biome_btn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Biome_btn : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+	public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Biome_btn");
+    }
+}
---
+++ Scripts/UI/Proie_btn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Proie_btn : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+	public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Proie_btn");
+    }
+}
